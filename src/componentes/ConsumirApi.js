07c5955c79d6f0fbe6a6ecac7cda6ffb3a64d1a8
BryanCevallosCruz <bryancevac@outlook.com>
2022-12-27 12:29:33 -0500

    mensaje-sin-gifs2

--- src/componentes/ConsumirApi.js
+++ src/componentes/ConsumirApi.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import clientHttp from "../services/ClientHttp";
-import { useNavigate, Link} from "react-router-dom";
+import { useNavigate} from "react-router-dom";
 
 const { useEffect, useState } = require("react");
 
@@ -16,27 +16,27 @@ const ConsumirApi = () => {
             .then((response) => {
                 setLista(response.data)
             });
-        setLoading(lista.length==0 ? false:true);
+        setLoading(lista.length == 0 ? false: true);
         console.log(lista.length)
-        console.log(loading) 
+        console.log(loading)
     }, []);
 
     const handlerCrear = () => {
         navegacion(`/*`)
-        clientHttp.post(`/`, {"url":descripcion.url, "author_id": autor})
-             .then(() => {
+        clientHttp.post(`/`, { "url": descripcion.url, "author_id": autor })
+            .then(() => {
                 navegacion(`/`)
-             });
-        setLoading(lista.length==0 ? false:true); 
+            });
+        setLoading(lista.length == 0 ? false : true);
     }
 
     const handlerEliminar = (item) => {
         navegacion(`/*`)
-        clientHttp.delete(`/`, {data:{"id": item.id, "url": item.url, "author_id": autor}})
+        clientHttp.delete(`/`, { data: { "id": item.id, "url": item.url, "author_id": autor } })
             .then(() => {
                 navegacion(`/`)
             });
-        setLoading(lista.length==0 ? false:true); 
+        setLoading(lista.length == 0 ? false : true);
     }
     const handleChange = (event) => {
         const target = event.target;
@@ -45,39 +45,40 @@ const ConsumirApi = () => {
         setDescripcion((tareaCurrent) => ({ ...tareaCurrent, [name]: value }));
     };
 
-    
-    return (loading ? <h1 className="container text-center">No posee gifs</h1>:
-          <><div className="container text-center border-bottom border-4">
-                <div className="row justify-content-md-center">
-                    <form className="row justify-content-md-center" onSubmit={(e) => handlerCrear(e)}>
+
+    return (loading ? <h1 className="container text-center">No posee gifs</h1> :
+        <><div className="container text-center border-bottom border-4">
+            <div className="row justify-content-md-center">
+                <form className="row justify-content-md-center" onSubmit={(e) => handlerCrear(e)}>
                     <div className="col col-lg-5">
                         <input type="text" className="form-control" id="url" required maxLength="500" placeholder="Gif URL"
                             onChange={e => handleChange(e)} />
                     </div>
                     <div className="col col-lg-1">
                         <button type="submit" style={{ backgroundColor: "white", borderRadius: "4px" }}
-                            >Agregar</button>
+                        >Agregar</button>
                     </div>
-                    </form>
-                </div>
-                    {lista.map((item) =>
-                        <div className="row justify-content-md-center" 
-                                style={{padding:"0.5em", 
-                                        border:"solid",     
-                                        borderColor:"white", 
-                                        borderRadius: "12px"}} 
-                                key={item.id}> 
-        
-                            <div className="col col-lg-4"> 
-                                <img className = "img-fluid mx-auto d-block" src={item.url} />
-                                <p/>
-                                <button type="button" className="btn btn-primary" aling="left"
-                                onClick={(e) => handlerEliminar(item)}>Eliminar</button>
-                                <p/>
-                            </div>                  
-                        </div>)}
+                </form>
             </div>
+            {lista.map((item) =>
+                <div className="row justify-content-md-center"
+                    style={{
+                        padding: "0.5em",
+                        border: "solid",
+                        borderColor: "white",
+                        borderRadius: "12px"
+                    }}
+                    key={item.id}>
 
+                    <div className="col col-lg-4">
+                        <img className="img-fluid mx-auto d-block" src={item.url} />
+                        <p />
+                        <button type="button" className="btn btn-primary" aling="left"
+                            onClick={(e) => handlerEliminar(item)}>Eliminar</button>
+                        <p />
+                    </div>
+                </div>)}
+        </div>
         </>
 
     );
